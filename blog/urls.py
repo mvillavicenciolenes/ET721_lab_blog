e0ef6fe6ee021274d6eaeb6daa17ec71d7ae5ed4
mvillavicenciolenes <143456253+mvillavicenciolenes@users.noreply.github.com>
2024-11-15 19:03:05 +0000

    fixed path with professor

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,7 @@
 # blog/urls.py
-from django.urls import path
+from django.urls import path, include
 from . import views
  
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:id>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_create, name='post_create'),
-    path('post/<int:id>/edit/', views.post_edit, name='post_edit'),
+    path('', views.index, name='index'),
 ]

8ac2a48c59f694e6d27ee53bb39eebea38399447
mvillavicenciolenes <143456253+mvillavicenciolenes@users.noreply.github.com>
2024-11-15 18:40:36 +0000

    incomplete part 3-4

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,5 @@
+# blog/tests.py
+
 from django.test import TestCase
 
 # Create your tests here.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
